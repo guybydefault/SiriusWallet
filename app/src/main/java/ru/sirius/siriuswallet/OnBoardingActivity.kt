202e2f1d451c891c1ced8edd43db076b92345fc9
user <artyom.ch.ch>
2021-08-18 20:46:05 +0300

    fix and auth in google

--- app/src/main/java/ru/sirius/siriuswallet/OnBoardingActivity.kt
+++ app/src/main/java/ru/sirius/siriuswallet/OnBoardingActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
-
-
 import android.content.Intent
 import android.view.View
 import androidx.activity.result.ActivityResult
@@ -19,20 +17,6 @@ class OnBoardingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOnboardingBinding
 
-//    val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//        .requestEmail()
-//        .build()
-//
-//
-//    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//        if (result.resultCode == Activity.RESULT_OK) {
-//            val data: Intent? = result.data
-//        }
-//    }
-
-    //val  mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-
-
     private val loginResultHandler =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
 
@@ -42,15 +26,9 @@ class OnBoardingActivity : AppCompatActivity() {
             startSecondActivity(account)
         }
 
-
-
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setTheme(R.style.Theme_SiriusWallet)
+        setTheme(R.style.Theme_SiriusWallet)
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -62,10 +40,8 @@ class OnBoardingActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-        startSecondActivity(account)
+        startSecondActivity(GoogleSignIn.getLastSignedInAccount(this))
     }
-
 
     private fun getSignInIntent(): Intent {
 
@@ -78,16 +54,13 @@ class OnBoardingActivity : AppCompatActivity() {
         return mGoogleSignInClient.signInIntent
     }
 
-
     private fun startSecondActivity(account: GoogleSignInAccount?) {
         if (account != null) {
             binding.identityButton.visibility = View.INVISIBLE
             val intent = Intent(this, MainActivity::class.java)
-            this.startActivity(intent)
-            // start second activity
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
-
-
 
 }
--- app/src/main/java/ru/sirius/siriuswallet/SplashActivity.kt
+++ app/src/main/java/ru/sirius/siriuswallet/SplashActivity.kt
@@ -8,12 +8,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        setTheme(R.style.Theme_SiriusWallet)
 
         val intent = Intent(this, OnBoardingActivity::class.java)
         this.startActivity(intent)
-
-
-
+        finish()
     }
 }
