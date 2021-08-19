84102539478d3592c6e12c016dc5c2c6878310b0
guybydefault <lexmint@rambler.ru>
2021-08-19 10:44:12 +0300

    Bunch of new activities for hw2 & xml fix

--- app/src/main/java/ru/sirius/siriuswallet/WalletInfoActivity.kt
+++ app/src/main/java/ru/sirius/siriuswallet/WalletInfoActivity.kt
@@ -1,10 +1,11 @@
 package ru.sirius.siriuswallet
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import ru.sirius.siriuswallet.databinding.ActivityWalletInfoBinding
 
 class WalletInfoActivity : AppCompatActivity() {
@@ -17,17 +18,16 @@ class WalletInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupListeners()
+        setContentView(binding.root)
+    }
+
+    private fun setupListeners() {
         binding.backArrowBtn.setOnClickListener { onBackClick(it) }
         binding.settingsBtn.setOnClickListener { onSettingsClick(it) }
         binding.addOperationBtn.setOnClickListener { onAddOperationBtnClick(it) }
-//        recyclerView = binding.walletRecyclerView
-//        recyclerView.apply {
-//            adapter = WalletRecyclerViewAdapter
-//            layoutManager = LinearLayoutManager(this@WalletList)
-//            setHasFixedSize(true)
-//        }
-        setContentView(binding.root)
     }
+
 
     fun onBackClick(view: View) {
         Toast.makeText(this, "Back arrow click", Toast.LENGTH_LONG).show()
@@ -38,7 +38,9 @@ class WalletInfoActivity : AppCompatActivity() {
     }
 
     fun onAddOperationBtnClick(view: View) {
-        Toast.makeText(this, "Add operation", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, SelectOperationCategoryActivity::class.java)
+        val options = ActivityOptions.makeSceneTransitionAnimation(this);
+        startActivity(intent, options.toBundle())
     }
 
 
