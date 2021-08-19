efb5d9c8cd3a33dee4bb76a4c8c2a523718fa8ed
user <artyom.ch.ch>
2021-08-19 10:27:19 +0300

    full screen

--- app/src/main/java/ru/sirius/siriuswallet/SplashActivity.kt
+++ app/src/main/java/ru/sirius/siriuswallet/SplashActivity.kt
@@ -1,13 +1,13 @@
 package ru.sirius.siriuswallet
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_SiriusWallet)
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         val intent = Intent(this, OnBoardingActivity::class.java)
         this.startActivity(intent)
