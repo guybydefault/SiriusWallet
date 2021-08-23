27fcc9c2ece91a55fd8c2d23d91d592404cc5990
guybydefault <lexmint@rambler.ru>
2021-08-23 10:32:18 +0300

    Unused import

--- app/src/main/java/ru/sirius/siriuswallet/operations/OperationsViewModel.kt
+++ app/src/main/java/ru/sirius/siriuswallet/operations/OperationsViewModel.kt
@@ -5,12 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ru.sirius.siriuswallet.R
 import ru.sirius.siriuswallet.SiriusWalletContainer
 import ru.sirius.siriuswallet.dao.Response
 import ru.sirius.siriuswallet.model.Operation
-import java.math.BigDecimal
-import java.time.LocalDateTime
 
 @SuppressLint("NewApi")
 class OperationsViewModel(container: SiriusWalletContainer) : ViewModel() {
