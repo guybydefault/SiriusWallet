c79ea068aaff5a5bac7d0f6e75ea93ca1dcb4fbb
guybydefault <lexmint@rambler.ru>
2021-08-25 15:09:25 +0300

    Get categories by type

--- app/src/main/java/ru/sirius/siriuswallet/data/local/dao/CategoryWithOperationsDao.kt
+++ app/src/main/java/ru/sirius/siriuswallet/data/local/dao/CategoryWithOperationsDao.kt
@@ -2,6 +2,7 @@ package ru.sirius.siriuswallet.data.local.dao
 
 import androidx.room.*
 import ru.sirius.siriuswallet.data.local.entities.CategoryEntity
+import ru.sirius.siriuswallet.data.local.entities.DatabaseCategoryType
 import ru.sirius.siriuswallet.data.local.entities.OperationEntity
 import ru.sirius.siriuswallet.data.local.entities.OperationWithCategory
 
@@ -10,6 +11,9 @@ abstract class CategoryWithOperationsDao {
     @Query("SELECT * FROM operation o JOIN category c ON c.id = o.operationCategoryId WHERE userId = :userId")
     @Transaction
     abstract fun getOperationsByUserId(userId: Int): List<OperationWithCategory>
+
+    @Query("SELECT * FROM category c WHERE c.categoryType = :type")
+    abstract fun getCategoriesByType(type: DatabaseCategoryType): List<CategoryEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract fun insertCategory(categoryEntity: CategoryEntity)
--- app/src/main/java/ru/sirius/siriuswallet/data/local/repository/CategoryLocalRepository.kt
+++ app/src/main/java/ru/sirius/siriuswallet/data/local/repository/CategoryLocalRepository.kt
@@ -3,11 +3,15 @@ package ru.sirius.siriuswallet.data.local.repository
 import ru.sirius.siriuswallet.data.CategoryRepository
 import ru.sirius.siriuswallet.data.Response
 import ru.sirius.siriuswallet.data.local.Database
+import ru.sirius.siriuswallet.data.toCategory
+import ru.sirius.siriuswallet.data.toCategoryDBType
 import ru.sirius.siriuswallet.model.Category
 import ru.sirius.siriuswallet.model.CategoryType
 
-class CategoryLocalRepository(db: Database) : CategoryRepository {
+class CategoryLocalRepository(private val db: Database) : CategoryRepository {
     override suspend fun getCategoriesByType(categoryType: CategoryType): Response<List<Category>> {
-        TODO("Not yet implemented")
+        return Response.Success(
+            db.categoryWithOperationsDao().getCategoriesByType(categoryType.toCategoryDBType())
+                .map { it.toCategory() })
     }
 }
